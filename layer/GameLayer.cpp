7dcdcb091b7d05787471206178976fbc4c8153a1
mysteriousdog <3076777685@qq.com>
2020-11-26 23:46:30 +0800

    to gamelayer.cpp

--- layer/GameLayer.cpp
+++ layer/GameLayer.cpp
@@ -7,7 +7,7 @@ GameLayer::GameLayer(void)
 
 GameLayer::~GameLayer(void)
 {
-	this->unscheduleAllSelectors();
+	this->unscheduleAllCallbacks();
 }
 
 bool GameLayer::init()
@@ -28,7 +28,7 @@ bool GameLayer::init()
 		//将Hero加入GameLayer
 		addChild(hero, kZHero, kZHero);
 
-		schedule(schedule_selector(GameLayer::update));
+		schedule(CC_SCHEDULE_SELECTOR(GameLayer::update));
 
         bRet = true;
     } while (0);
@@ -36,7 +36,7 @@ bool GameLayer::init()
     return bRet;
 }
 
-void GameLayer::update(ccTime dt)
+void GameLayer::update(float dt)
 {
 	//如果勇士不在行走状态，不需要更新场景位置
 	if (hero->isHeroMoving)
@@ -46,12 +46,13 @@ void GameLayer::update(ccTime dt)
 }
 
 //传入勇士当前位置信息，将场景移动到相应位置
-void GameLayer::setSceneScrollPosition(CCPoint position)
+void GameLayer::setSceneScrollPosition(Point position)
 {
 	//获取屏幕尺寸
-	CCSize screenSize = CCDirector::sharedDirector()->getWinSize();
+	//Size screenSize = CCDirector::sharedDirector()->getWinSize();
+	auto screenSize = Director::getInstance()->getWinSize();
 	//计算Tilemap的宽高，单位是像素
-	CCSize mapSizeInPixel = CCSizeMake(map->getMapSize().width * map->getTileSize().width, 
+	auto mapSizeInPixel = Size(map->getMapSize().width * map->getTileSize().width, 
 		map->getMapSize().height * map->getTileSize().height);
 	//取勇士当前x坐标和屏幕中点x的最大值，如果勇士的x值较大，则会滚动
 	float x = MAX(position.x, screenSize.width / 2.0f);
@@ -67,35 +68,36 @@ void GameLayer::setSceneScrollPosition(CCPoint position)
 		y = MIN(y, mapSizeInPixel.height - screenSize.height / 2.0f);
 	}
 	//勇士的实际位置
-	CCPoint heroPosition = ccp(x, y);
+	Point heroPosition = Vec2(x, y);
 	//屏幕中点位置
-	CCPoint screenCenter = ccp(screenSize.width/2.0f, screenSize.height/2.0f);
+	Point screenCenter = Vec2(screenSize.width/2.0f, screenSize.height/2.0f);
 	//计算勇士实际位置和重点位置的距离
-	CCPoint scrollPosition = ccpSub(screenCenter, heroPosition);
+	Point scrollPosition = screenCenter - heroPosition;
 	//将场景移动到相应位置
 	this->setPosition(scrollPosition);
-	CCLog("%f,%f", scrollPosition.x, scrollPosition.y);
+	CCLOG("%f,%f", scrollPosition.x, scrollPosition.y);
 }
 
 //显示提示信息
-void GameLayer::showTip(const char *tip, CCPoint startPosition)
+void GameLayer::showTip(const char *tip, Point startPosition)
 {
 	//新建一个文本标签
-	CCLabelTTF *tipLabel = CCLabelTTF::labelWithString(tip, "Arial", 20);
-	tipLabel->setPosition(ccpAdd(startPosition, ccp(16, 16)));
+	auto tipLabel = Label::createWithTTF(tip, "fonts/Marker Felt.ttf", 24);
+	tipLabel->setPosition(startPosition + Vec2(16, 16));
 	this->addChild(tipLabel, kZTip,kZTip);
 	//定义动画效果
-	CCAction* action = CCSequence::actions(
-		CCMoveBy::actionWithDuration(0.5f, ccp(0, 32)),
-		CCDelayTime::actionWithDuration(0.5f),
-		CCFadeOut::actionWithDuration(0.2f),
-		CCCallFuncN::actionWithTarget(this, callfuncN_selector(GameLayer::onShowTipDone)),
+	auto action = Sequence::create(
+		MoveBy::create(0.5f, Vec2(0, 32)),
+		DelayTime::create(0.5f),
+		FadeOut::create(0.2f),
+		CC_CALLBACK_1(GameLayer::onShowTipDone, this),
+		//CCCallFuncN::actionWithTarget(this, callfuncN_selector(GameLayer::onShowTipDone)),
 		NULL);
 	tipLabel->runAction(action);
 }
 
 //提示消息显示完后的回调
-void GameLayer::onShowTipDone(CCNode* pSender)
+void GameLayer::onShowTipDone(Ref* pSender)
 {
 	//删掉文本标签
 	this->getChildByTag(kZTip)->removeFromParentAndCleanup(true);
