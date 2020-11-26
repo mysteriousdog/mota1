073eae8ce4f568fd1bfe0a97b3a59598e86b0dc7
mysteriousdog <3076777685@qq.com>
2020-11-27 00:19:45 +0800

    to gamescene.cpp

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -12,17 +12,17 @@ GameScene::~GameScene(void)
 bool GameScene::init()
 {
 	//新建一个GameLayer实例
-	GameLayer *gamelayer = GameLayer::node();
+	GameLayer *gamelayer = GameLayer::create();
 	//将GameLayer实例添加到场景中
 	this->addChild(gamelayer, kGameLayer, kGameLayer);
 	//新建一个ControlLayer实例
-	ControlLayer *controlLayer = ControlLayer::node();
+	ControlLayer *controlLayer = ControlLayer::create();
 	//将ControlLayer实例添加到场景中
 	this->addChild(controlLayer, kControlLayer, kControlLayer);
 	return true;
 }
 
-CCScene* GameScene::playNewGame()
+Scene* GameScene::playNewGame()
 {
 	GameScene * scene = NULL;
 	do 
@@ -30,8 +30,8 @@ CCScene* GameScene::playNewGame()
 		//新游戏，当前地图层数为0
 		sGlobal->currentLevel = 0;
 		//勇士出生位置
-		sGlobal->heroSpawnTileCoord = ccp(1, 11);
-		scene = GameScene::node();
+		sGlobal->heroSpawnTileCoord = Vec2(1, 11);
+		scene = GameScene::create();
 		CC_BREAK_IF(! scene);
 	} while (0);
 	return scene;
@@ -40,14 +40,15 @@ CCScene* GameScene::playNewGame()
 void GameScene::switchMap()
 {
 	//创建一个遮罩层，用于地图切换时的显示淡入淡出效果
-	CCLayerColor* fadeLayer = CCLayerColor::layerWithColor(ccc4(0, 0, 0, 0));
-	fadeLayer->setAnchorPoint(CCPointZero);
-	fadeLayer->setPosition(CCPointZero);
+	auto fadeLayer = LayerColor::create(Color4B(0, 0, 0, 0));
+	fadeLayer->setAnchorPoint(Vec2(0, 0));
+	fadeLayer->setPosition(Vec2(0, 0));
 	addChild(fadeLayer, kFadeLayer, kFadeLayer);
 	//执行淡入动画，结束后调用resetGameLayer方法
-	CCAction* action = CCSequence::actions(
-		CCFadeIn::actionWithDuration(0.5f),
-		CCCallFunc::actionWithTarget(this, callfunc_selector(GameScene::resetGameLayer)),
+	auto action = Sequence::create(
+		FadeIn::create(0.5f),
+		CC_CALLBACK_1(GameScene::resetGameLayer, this),
+		//CCCallFunc::actionWithTarget(this, callfunc_selector(GameScene::resetGameLayer)),
 		NULL);
 	fadeLayer->runAction(action);
 }
@@ -57,12 +58,13 @@ void GameScene::resetGameLayer()
 	//删除老的GameLayer
 	this->removeChildByTag(kGameLayer, true);
 	//创建新的GameLayer
-	GameLayer *gamelayer = GameLayer::node();
+	GameLayer *gamelayer = GameLayer::create();
 	this->addChild(gamelayer, kGameLayer, kGameLayer);
 	//遮罩层执行淡出效果，结束后，调用removeFadeLayer方法删除遮罩层
-	CCAction* action = CCSequence::actions(
-		CCFadeOut::actionWithDuration(0.5f),
-		CCCallFunc::actionWithTarget(this, callfunc_selector(GameScene::removeFadeLayer)),
+	auto action = Sequence::create(
+		FadeOut::create(0.5f),
+		CC_CALLBACK_1(GameScene::removeFadeLayer, this),
+		//CCCallFunc::actionWithTarget(this, callfunc_selector(GameScene::removeFadeLayer)),
 		NULL);
 	this->getChildByTag(kFadeLayer)->runAction(action);
 }
